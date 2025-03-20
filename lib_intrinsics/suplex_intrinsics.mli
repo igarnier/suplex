e33b4a4ee2d1f26baad8ab56c83d365c2b300ab7
Ilias Garnier <igarnier@protonmail.com>
2025-03-20 22:26:19 +0100

    Intrinsics: add some comments

--- lib_intrinsics/suplex_intrinsics.mli
+++ lib_intrinsics/suplex_intrinsics.mli
@@ -5,36 +5,54 @@ module Vector : sig
     (** [op] describes reduction operations. *)
     type op
 
+    (** [add] is the integer addition operation. *)
     val add : op
 
+    (** [mul] is the integer multiplication operation. *)
     val mul : op
 
+    (** [fadd] is the floating-point addition operation. *)
     val fadd : op
 
+    (** [fsub] is the floating-point subtraction operation. *)
     val fmul : op
 
+    (** [and_] is the bitwise AND operation. *)
     val and_ : op
 
+    (** [or_] is the bitwise OR operation. *)
     val or_ : op
 
+    (** [xor] is the bitwise XOR operation. *)
     val xor : op
 
+    (** [smax] is the signed maximum operation. *)
     val smax : op
 
+    (** [smin] is the signed minimum operation. *)
     val smin : op
 
+    (** [umax] is the unsigned maximum operation. *)
     val umax : op
 
+    (** [umin] is the unsigned minimum operation. *)
     val umin : op
 
+    (** [fmax] is the floating-point maximum operation. *)
     val fmax : op
 
+    (** [fmin] is the floating-point minimum operation. *)
     val fmin : op
 
+    (** [fmaximum] is the floating-point maximum operation, and propagates NaNs.
+    *)
     val fmaximum : op
 
+    (** [fminimum] is the floating-point minimum operation, and propagates NaNs.
+    *)
     val fminimum : op
 
+    (** [string_of_op op] is a string representation of the operation [op]. *)
     val string_of_op : op -> string
 
     (** [reduce op numty len] is a vector reduction intrinsic for the operation

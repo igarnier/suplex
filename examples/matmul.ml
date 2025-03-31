3440a6ba798e6cb17e10a0978b3c1ea246feb39f
Ilias Garnier <igarnier@protonmail.com>
2025-03-31 21:29:42 +0200

    WIP

--- examples/matmul.ml
+++ examples/matmul.ml
@@ -25,12 +25,11 @@ let matmul a b res =
     @@ fun _self a_data a_rows a_cols b_data _b_rows b_cols out_data ->
       let* inner = a_cols in
 
-      (* Allocate result matrix *)
       let* a_data = a_data.%{F32_ba.data} in
       let* b_data = b_data.%{F32_ba.data} in
       let* res = out_data.%{F32_ba.data} in
 
-      let get_a_elt row col = a_data.%[I64.(add col (mul row inner))] in
+      let get_a_elt row col = a_data.%[I64.(add col (mul row a_cols))] in
       let get_b_elt row col = b_data.%[I64.(add col (mul row b_cols))] in
 
       (* Initialize result to zero *)
@@ -42,7 +41,7 @@ let matmul a b res =
       (*     (fun i -> res.%[i] <- F32.zero) *)
       (* in *)
       let* block_size = I64.v 8L in
-      let for_each start stop =
+      let _for_each_block start stop =
         for_
           ~init:start
           ~pred:(fun i -> I64.lt i stop)
@@ -51,11 +50,19 @@ let matmul a b res =
       for_each I64.zero b_cols (fun i ->
           for_each I64.zero a_rows (fun j ->
               let* res_idx = I64.add (I64.mul j b_cols) i in
-              for_each I64.zero inner (fun k ->
-                  let* a_val = get_a_elt j k in
-                  let* b_val = get_b_elt j i in
-                  let* current = res.%[res_idx] in
-                  res.%[res_idx] <- F32.add current (F32.mul a_val b_val)))) )
+              let* acc = res.%[res_idx] in
+              let* tot =
+                foldi
+                  ~init:I64.zero
+                  ~acc
+                  ~pred:(fun k _acc -> I64.lt k inner)
+                  ~step:succ
+                  (fun k acc ->
+                    let* a_val = get_a_elt j k in
+                    let* b_val = get_b_elt k i in
+                    F32.add acc (F32.mul a_val b_val))
+              in
+              res.%[res_idx] <- tot)) )
     a.ba
     a.rows
     a.cols
@@ -89,7 +96,7 @@ let ocaml_matmul a b res =
 
 (* Benchmarking function *)
 let benchmark () =
-  let size = 1024 in
+  let size = 128 in
   (* Test with 64x64 matrices *)
   let mat1 =
     { rows = Int64.of_int size;
@@ -111,7 +118,7 @@ let benchmark () =
           (Array.init (size * size) (fun i -> float_of_int (i + 1) /. 100.0))
     }
   in
-  let res =
+  let res () =
     { rows = Int64.of_int size;
       cols = Int64.of_int size;
       ba =
@@ -123,16 +130,29 @@ let benchmark () =
   in
 
   let time fn =
+    let res = res () in
     let start = Unix.gettimeofday () in
-    let _ = fn mat1 mat2 res in
-    Unix.gettimeofday () -. start
+    let () = fn mat1 mat2 res in
+    (res, Unix.gettimeofday () -. start)
   in
 
-  let ocaml_time = time ocaml_matmul in
-  let suplex_time = time matmul in
+  let (res_ocaml, ocaml_time) = time ocaml_matmul in
+  let (res_suplex, suplex_time) = time matmul in
+
+  let l1_dist mat1 mat2 =
+    let acc = ref 0.0 in
+    for i = 0 to Array1.dim mat1.ba - 1 do
+      let v1 = mat1.ba.{i} in
+      let v2 = mat2.ba.{i} in
+      let v = v1 -. v2 in
+      acc := !acc +. abs_float v
+    done ;
+    !acc
+  in
 
   Printf.printf "OCaml implementation: %f seconds\n" ocaml_time ;
   Printf.printf "Suplex implementation: %f seconds\n" suplex_time ;
+  Printf.printf "Dist: %.2f\n" (l1_dist res_ocaml res_suplex) ;
   Printf.printf "Speedup: %.2fx\n" (ocaml_time /. suplex_time)
 
 let () = benchmark ()
